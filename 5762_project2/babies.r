e9391d67b488ef6f0e5e20898476ca37f49b8b76
Christopher Kromm <cpkk@st-andrews.ac.uk>
2019-10-20 18:53:53 +0100

    Revised getting the text name code and added code to slip the data.

--- 5762_project2/babies.r
+++ 5762_project2/babies.r
@@ -1,35 +1,27 @@
+require(tidyverse)
 
-
-#load data
-baby <- read.table("../../5762_project2/5762_project2/babies23.data")
-metadata <- read.delim("../../5762_project2/5762_project2/babies.readme.txt")
+#load dataDocuments\5762_project2\5762_project2\Mt5762_regression
+baby <- read.table("C:\\Users\\Christopher\\Documents\\5762_project2\\5762_project2\\Mt5762_regression\\5762_project2\\babies23.data")
+metadata <- read.delim("C:\\Users\\Christopher\\Documents\\5762_project2\\5762_project2\\Mt5762_regression\\5762_project2\\babies.readme.txt")
 
 #remae column
 colnames(metadata)<-c("label")
 
-#get names from titles
-metalab1 <- sapply(1:nrow(metadata), function(ox) unlist(strsplit(as.character(metadata[ox, 1]),  "-")[1])[1])
-metalab2 <- sapply(1:nrow(metadata), function(p) unlist(strsplit(metalab1[p], " ")), simplify = "array")
+#grab rows of interest.
+metalab1 <- sapply(1:nrow(metadata), function(ox) unlist(strsplit(as.character(metadata[ox, 1]),  "-" )[1])[1])
+metalab1_index <- sapply(1:nrow(metadata), function(ox) unlist(strsplit(as.character(metadata[ox, 1]),  " " )[1])[1])
 
-#intialise loop
-labels <-vector()
-i <-0
-for (td in 1:nrow(metadata)) {
-    #get only the titles for the variables.
-    labels1 <- metalab2[[td]]
-    labels2 <-list(strsplit(labels1, " "))
-    if(length(unlist(labels2)) == 2){
-      i <- i + 1
-      labels[i] <- list(strsplit(unlist(labels2), " ")[2])
-    }
-  
-}
+#get only the 23 names (ignore error message)
+metalab1_names <- metalab1[which(!is.na(as.numeric(metalab1_index)))]
+metalab2_names <- sapply(1:length(metalab1_names), function(p) unlist(strsplit(metalab1_names[p], " "))[2], simplify = "array")
+
 
 #add the "correct" names.
-colnames(baby) <- unlist(labels)
+colnames(baby) <- metalab2_names
+
 
 #change names!
-
+colnames(baby)[7] <- "Chien"
 
 
 #the names are wrong!
@@ -48,4 +40,30 @@ incomplete_records <- baby[!complete_c,]
 nrow(complete_records) + nrow(incomplete_records)==nrow(baby)
 #There are no incomplete (missing implicity) values
 
+#split data by id (because babies are the sampling unit).
+
+#TEST SET
+#get a random number so my random number generator is deterministic.
+set.seed(095)
+
+#The test set baby weights.
+test_set <- as.data.frame(sample(unique(baby[, 1]) , round((length(baby[, 1])*0.2))))
+colnames(test_set) <- "id"
+
+#TRAINING
+#get training dataset
+training_set<-as.data.frame(unique(baby[, 1])[which(is.na(match(unique(baby[, 1]), test_set[, 1])))])
+colnames(training_set) <- "id"
+
+#check it worked.
+nrow(training_set) + nrow(test_set) == length(unique(baby[, 1]))
+
+#Analysis data
+schooling <- semi_join(baby, training_set, by = "id")
+
+#save test data for after the model is built. 
+test_data <- semi_join(baby, test_set, by = "id")
+
+#check no child left behind.
+nrow(test_data) + nrow(training_set) == nrow(baby)
 
