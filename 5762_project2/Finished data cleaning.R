6403b1b90721fb55293b8cd1eb7ee7057a7727c3
Christopher Kromm <cpkk@st-andrews.ac.uk>
2019-10-30 17:25:39 +0000

    Fixed the date error.

--- 5762_project2/Finished data cleaning.R
+++ 5762_project2/Finished data cleaning.R
@@ -80,7 +80,7 @@ baby <- baby[-grep('outcome', colnames(baby))]
 baby <- baby[-grep('sex', colnames(baby))]
 
 #Convert dates to actual date format
-baby$date <- as.Date((date-1096), origin = "1961-01-01")
+baby$date <- as.Date((baby$date-1096), origin = "1961-01-01")
 
 #Replace all unknowns with NA as per descriptor readme values
 baby$gestation[baby$gestation == "999"] <- NA
