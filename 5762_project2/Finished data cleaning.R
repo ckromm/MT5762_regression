fe9bacfba7a5a1387b0a574039b8435740d00ae8
Christopher Kromm <cpkk@st-andrews.ac.uk>
2019-10-30 17:01:59 +0000

    Imputed values and fix some errors.

--- 5762_project2/Finished data cleaning.R
+++ 5762_project2/Finished data cleaning.R
@@ -33,17 +33,46 @@ colnames(baby)[11] <- "med"
 colnames(baby)[12] <- "mht" 
 colnames(baby)[13] <- "mwt"
 
+#Replace all unknowns with NA as per descriptor readme values
+baby$gestation[baby$gestation == "999"] <- NA
+baby$mrace[baby$mrace == "99"] <- NA
+baby$mage[baby$mage == "99"] <- NA
+baby$med[baby$med == "9"] <- NA
+baby$mht[baby$mht == "99"] <- NA
+baby$mwt[baby$mwt == "999"] <- NA
+baby$drace[baby$drace == "99"] <- NA
+baby$dage[baby$dage == "99"] <- NA
+baby$ded[baby$ded == "9"] <- NA
+baby$dht[baby$dht == "99"] <- NA
+baby$dwt[baby$dwt == "999"] <- NA
+baby$marital[baby$marital == "0"] <- NA
+baby$inc[baby$inc == "98"] <- NA
+baby$smoke[baby$smoke == "9"] <- NA
+baby$time[baby$time == "99"] <- NA
+baby$time[baby$time == "98"] <- NA  
+baby$number[baby$number == "98"] <- NA
+baby$number[baby$number == "9"] <- NA  
+
 #check data types
 str(baby)
 
 #Convert variables to factors
-baby$id <- as.factor(baby$id)
-baby$pluralty <- as.factor(baby$pluralty)
-baby$outcome <- as.factor(baby$outcome)
-baby$date <- as.factor(baby$date)
+baby$id <- as.factor(as.character(baby$id))
+baby$pluralty <- as.factor(as.character(baby$pluralty))
+baby$outcome <- as.factor(as.character(baby$outcome))
 baby$gestation <- as.numeric(baby$gestation)
-baby$sex <- as.factor(baby$sex)
-baby$parity <- as.factor(baby$parity)
+baby$sex <- as.factor(as.character(baby$sex))
+baby$parity <- as.factor(as.character(baby$parity))
+baby$mrace <- as.factor(as.character(baby$mrace))
+baby$med <- as.factor(as.character(baby$med))
+baby$drace <- as.factor(as.character(baby$mrace))
+baby$ded <- as.factor(as.character(baby$ded))
+baby$marital <- as.factor(as.character(baby$marital))
+baby$inc <- as.factor(as.character(baby$inc))
+baby$smoke <- as.factor(as.character(baby$smoke))
+baby$time <- as.factor(as.character(baby$time))
+baby$number <- as.factor(as.character(baby$number))
+
 
 #Remove columns that don't show useful information/are all the same
 baby <- baby[-grep('pluralty', colnames(baby))] 
@@ -82,6 +111,33 @@ baby$ded[baby$ded == 7] <- 6
 #find na value
 find_na <- apply(baby, 2, is.na)
 impute <- names(which(apply(find_na, 2, any)))
+impute_dat <- baby %>% select(impute)
+
+for(z in 1: ncol(impute_dat)){
+ u <- impute_dat[ , z ]
+  type <- class(u)
+  if(type == "numeric"){
+    u[which(is.na(u))] = mean(u, na.rm = T)
+    
+  } else{
+    u[which(is.na(u))] <- which.max(table(u))
+
+  }
+  
+  impute_dat[ , z] <- u
+  
+  
+  
+}
+
+#check for na values.
+any(is.na(impute_dat))
+
+#join to the other data
+baby[, names(impute_dat)] <- impute_dat
+
+#check for na values.
+any(is.na(baby))
 
 
 ############################
