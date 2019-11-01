e909d0ba50fd37eeee6c6a473a4c47ebf1d5b53d
JKhandelwal <jk218@st-andrews.ac.uk>
2019-11-01 11:32:07 +0000

    model files

--- 5762_project2/model.r
+++ 5762_project2/model.r
@@ -1,17 +1,20 @@
 source("Finished data cleaning.R")
-
+library("DAAG")
 
 calculate_MSE <- function(model, test) {
   mse <- mean((test$bwt - predict.lm(model, test)) ^ 2)
   return(mse)
 }
 
+perform_cv <- function(model, data) {
+  cross_v<-cv.lm(data=data, model, m=3)
+}
+
 
 logical_model <- function(data) {
   print(head(data))
-  print("yeet")
 
-  model <- lm(bwt ~ smoke + time + number + gestation + mage + ded + med + mht + dht + mwt + mrace + inc +
+  model <- lm(bwt ~ smoke + number + time + gestation + mage + ded + med + mht + dht + mwt + mrace + inc +
             ded:med + mht:mwt + mrace:med + inc:ded + inc:med + inc:mwt , data=data)
 
   return(model)
@@ -19,5 +22,12 @@ logical_model <- function(data) {
 data <- get_data("babies23.data")
 
 model <- logical_model(data$train)
+
+summary(model)
 mse <- calculate_MSE(model, data$test)
 print(mse)
+perform_cv(model, data$train)
+
+t <-  data$train
+str(t[,"number"])
+class(t[,"number"])
