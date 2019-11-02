193112d5c35b5c32e5d032aa03b2f82f8f478653
JKhandelwal <jk218@st-andrews.ac.uk>
2019-11-02 11:17:28 +0000

    cv works, na's appear because of collinearity, so for modelling don't have collinear variables

--- 5762_project2/Finished data cleaning.R
+++ 5762_project2/Finished data cleaning.R
@@ -5,26 +5,26 @@
 library(tidyverse)
 
 get_data <- function(name) {
-  
+
   baby <- read.delim(name, sep="", header=TRUE)
-  
-  
+
+
   #check for na values
   any(which(is.na(baby)))
   #There are no na value
-  
+
   #check for missing values
   complete_c <- complete.cases(baby)
   complete_records <- baby[complete_c,]
   incomplete_records <- baby[!complete_c,]
-  
+
   #check is the row match
   nrow(complete_records) + nrow(incomplete_records)==nrow(baby)
   #There are no incomplete (missing implicity) values
-  
+
   #see names
   names(baby)
-  
+
   #Change names to be more descriptive
   colnames(baby)[7] <- "bwt"
   colnames(baby)[9] <- "mrace"
@@ -32,7 +32,7 @@ get_data <- function(name) {
   colnames(baby)[11] <- "med"
   colnames(baby)[12] <- "mht"
   colnames(baby)[13] <- "mwt"
-  
+
   #Replace all unknowns with NA as per descriptor readme values
   baby$gestation[baby$gestation == "999"] <- NA
   baby$mrace[baby$mrace == "99"] <- NA
@@ -52,16 +52,16 @@ get_data <- function(name) {
   baby$time[baby$time == "98"] <- NA
   baby$number[baby$number == "98"] <- NA
   baby$number[baby$number == "9"] <- NA
-  
+
   #Combine values which are the same
   baby$mrace[c((baby$mrace == 0) | (baby$mrace == 1) | (baby$mrace == 2) | (baby$mrace == 3) | (baby$mrace == 4))] <- 5
   baby$drace[c((baby$drace == 0) | (baby$drace == 1) | (baby$drace == 2) | (baby$drace == 3) | (baby$drace == 4))] <- 5
   baby$med[baby$med == 7] <- 6
   baby$med[baby$med == 7] <- 6
-  
+
   #check data types
   str(baby)
-  
+
   #Convert variables to factors
   baby$id <- as.factor(as.character(baby$id))
   baby$pluralty <- as.factor(as.character(baby$pluralty))
@@ -78,116 +78,117 @@ get_data <- function(name) {
   baby$smoke <- as.factor(as.character(baby$smoke))
   baby$time <- as.factor(as.character(baby$time))
   baby$number <- as.factor(as.character(baby$number))
-  
-  
+
+
   #Remove columns that don't show useful information/are all the same
   baby <- baby[-grep('pluralty', colnames(baby))]
   baby <- baby[-grep('outcome', colnames(baby))]
   baby <- baby[-grep('sex', colnames(baby))]
-  
+
   #Convert dates to actual date format
   baby$date <- as.Date((baby$date-1096), origin = "1961-01-01")
-  
+
   #impute for missing values
   #find na value
   find_na <- apply(baby, 2, is.na)
   impute <- names(which(apply(find_na, 2, any)))
   impute_dat <- baby[c(impute)]
-  
+
   df_ed_racem<-as.data.frame(cbind(rownames(table(baby$mrace, baby$med)),
                             colnames(table(baby$mrace, baby$med))[apply(table(baby$mrace, baby$med), 1,which.max)]))
   colnames(df_ed_racem)<-c("race", "edu")
-  
+
   df_ed_raced<-as.data.frame(cbind(rownames(table(baby$drace, baby$ded)),
                                    colnames(table(baby$drace, baby$ded))[apply(table(baby$drace, baby$ded), 1,which.max)]))
   colnames(df_ed_raced)<-c("race", "edu")
-  
+
   for(z in 1: ncol(impute_dat)){
     u <- impute_dat[ , z ]
     type <- class(u)
     if(type == "numeric" | type == "integer"){
       #impute the mean
       u[which(is.na(u))] = mean(u, na.rm = T)
-      
+
     }  else{
-           if(z == 7){
-             #drace
-             
-             #grab educartion at na
-             edu <- impute_dat[which(is.na(u)) , "ded" ]
-             for(uu in 1:length(edu)){
-               edui <- edu[uu]
-               #get race value for edu
-               rv <- sample( df_ed_raced$race[which(!is.na(match(df_ed_raced$edu, edui)))], 1)
-               u[which(is.na(u)) & which(impute_dat$ded==edu) ] <- rv 
-             }
-             
-           }else{
-             if(z == 2){
-               #mrace
-               
-               #grab educartion at na
-               edu <- impute_dat[which(is.na(u)) , "med" ]
-               for(uu in 1:length(edu)){
-                 edui <- edu[uu]
-                 #get race value for edu
-                 rv <- sample( df_ed_racem$race[which(!is.na(match(df_ed_racem$edu, edui)))], 1)
-                 #df_ed_racem$race[which(df_ed_racem$edu==edui)]
-                 u[which(is.na(u)) & which(impute_dat$med==edu) ] <- rv 
-               }
-               }
-               
-             else{
+           # if(z == 7){
+           #   #drace
+           #
+           #   #grab educartion at na
+           #   edu <- impute_dat[which(is.na(u)) , "ded" ]
+           #   for(uu in 1:length(edu)){
+           #     edui <- edu[uu]
+           #     #get race value for edu
+           #     rv <- as.factor(sample( df_ed_raced$race[which(!is.na(match(df_ed_raced$edu, edui)))], 1))
+           #     u[which(is.na(u)) & which(impute_dat$ded==edu) ] <- rv
+           #   }
+           #
+           # }else{
+           #   if(z == 2){
+           #     #mrace
+           #
+           #     #grab educartion at na
+           #     edu <- impute_dat[which(is.na(u)) , "med" ]
+           #     for(uu in 1:length(edu)){
+           #       edui <- edu[uu]
+           #       #get race value for edu
+           #       rv <- as.factor(sample( df_ed_racem$race[which(!is.na(match(df_ed_racem$edu, edui)))], 1))
+           #       #df_ed_racem$race[which(df_ed_racem$edu==edui)]
+           #       u[which(is.na(u)) & which(impute_dat$med==edu) ] <- rv
+           #     }
+           #     }
+           #
+           #   else{
         #impute the mode
         u[which(is.na(u))] <- sample(as.factor(colnames(t(table(u)[which(table(u)==max(table(u)))]))), 1)
-        
-      }}}
-      
-    
-    
+
+      # }}
+    }
+
+
+
     impute_dat[ , z] <- u
-    
-    
-    
+
+
+
   }
-  
+
   #check for na values.
   any(is.na(impute_dat))
-  
+
   #join to the other data
   baby[, names(impute_dat)] <- impute_dat
-  
+
   #check for na values.
   any(is.na(baby))
-  
-  
+
+
   ############################
   #Test Set
   ############################
   #get a random number so my random number generator is deterministic.
   set.seed(095)
-  
+
   #The test set baby weights.
   test_set <- as.data.frame(sample(unique(baby[, 1]) , round((length(baby[, 1])*0.2))))
   colnames(test_set) <- "id"
-  
+
   #TRAINING
   #get training dataset
   training_set<-as.data.frame(unique(baby[, 1])[which(is.na(match(unique(baby[, 1]), test_set[, 1])))])
   colnames(training_set) <- "id"
-  
+
   #check it worked.
   nrow(training_set) + nrow(test_set) == length(unique(baby[, 1]))
-  
+
   #Analysis data
   schooling <- semi_join(baby, training_set, by = "id")
-  
+
   #save test data for after the model is built.
   test_data <- semi_join(baby, test_set, by = "id")
-  
+
   #check no child left behind.
   nrow(test_data) + nrow(training_set) == nrow(baby)
-  
+
   return(list(test = test_data, train = schooling))
-  
+
 }
--- 5762_project2/model.r
+++ 5762_project2/model.r
@@ -1,33 +1,63 @@
 source("Finished data cleaning.R")
 library("DAAG")
+library("caret")
 
 calculate_MSE <- function(model, test) {
   mse <- mean((test$bwt - predict.lm(model, test)) ^ 2)
   return(mse)
 }
 
-perform_cv <- function(model, data) {
-  cross_v<-cv.lm(data=data, model, m=3)
-}
+# perform_cv <- function(model, data) {
+#   cross_v<-cv.lm(data=data, model, m=3)
+# }
 
 
 logical_model <- function(data) {
-  print(head(data))
+  train.control <- trainControl(method = "cv", number = 5)
+  # model <- lm(bwt ~ smoke + number + time + gestation + mage + ded + med + mht + dht + mwt + mrace + inc +
+  #           ded:med + mht:mwt + mrace:med + inc:ded + inc:med + inc:mwt , data=data)
+  # model <- lm(, data=data)
 
-  model <- lm(bwt ~ smoke + number + time + gestation + mage + ded + med + mht + dht + mwt + mrace + inc +
-            ded:med + mht:mwt + mrace:med + inc:ded + inc:med + inc:mwt , data=data)
+  model <- train(bwt ~ smoke + number + gestation + mage + mht + dht + mwt + inc +
+           mht:mwt + inc:mwt , data = data, method = "lm",
+       trControl = train.control)
 
   return(model)
 }
 data <- get_data("babies23.data")
+# new_train <- data$train %>% select(-mrace, -drace)
+# model <- logical_model(new_train)
+# summary(model)
+# summary(data$test)
+# mse <- calculate_MSE(model, data$test %>% select(-drace))
+# print(mse)
+# perform_cv(model, new_train)
+
+levels(data$test[,"number"]) <- levels(data$train[,"number"])
+lapply(data$train, unique)
+lapply(data$test, unique)
+# unique(data$train[,"number"])
+#
+# model <- logical_model(data$train)
+# summary(model)
+# mse <- calculate_MSE(model, data$test)
+# print(mse)
+# perform_cv(model, data$train)
+#
+#
+# data$test %>% dplyr::mutate_all(as.factor) %>% str
+
+source("cleantest.R")
+data <- get_data("babies23.data")
+t <- data$train
+head(t)
+hist(as.numeric(t$mrace))
 
 model <- logical_model(data$train)
-
 summary(model)
 mse <- calculate_MSE(model, data$test)
 print(mse)
 perform_cv(model, data$train)
 
-t <-  data$train
-str(t[,"number"])
-class(t[,"number"])
+# Does model analysis
+plot(model)
