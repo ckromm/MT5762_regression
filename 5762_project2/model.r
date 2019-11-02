e04750f1c2389691cc7568085ee975d3323177c6
JKhandelwal <jk218@st-andrews.ac.uk>
2019-11-02 11:51:45 +0000

    cleaned stuff up

--- 5762_project2/model.r
+++ 5762_project2/model.r
@@ -3,20 +3,13 @@ library("DAAG")
 library("caret")
 
 calculate_MSE <- function(model, test) {
-  mse <- mean((test$bwt - predict.lm(model, test)) ^ 2)
-  return(mse)
+  pred <- predict(model, test)
+  return(postResample(pred = pred, obs = data$test[,"bwt"]))
 }
-
-# perform_cv <- function(model, data) {
-#   cross_v<-cv.lm(data=data, model, m=3)
-# }
 
 
 logical_model <- function(data) {
   train.control <- trainControl(method = "cv", number = 5)
-  # model <- lm(bwt ~ smoke + number + time + gestation + mage + ded + med + mht + dht + mwt + mrace + inc +
-  #           ded:med + mht:mwt + mrace:med + inc:ded + inc:med + inc:mwt , data=data)
-  # model <- lm(, data=data)
 
   model <- train(bwt ~ smoke + number + gestation + mage + mht + dht + mwt + inc +
            mht:mwt + inc:mwt , data = data, method = "lm",
@@ -24,40 +17,12 @@ logical_model <- function(data) {
 
   return(model)
 }
-data <- get_data("babies23.data")
-# new_train <- data$train %>% select(-mrace, -drace)
-# model <- logical_model(new_train)
-# summary(model)
-# summary(data$test)
-# mse <- calculate_MSE(model, data$test %>% select(-drace))
-# print(mse)
-# perform_cv(model, new_train)
 
-levels(data$test[,"number"]) <- levels(data$train[,"number"])
-lapply(data$train, unique)
-lapply(data$test, unique)
-# unique(data$train[,"number"])
-#
-# model <- logical_model(data$train)
-# summary(model)
-# mse <- calculate_MSE(model, data$test)
-# print(mse)
-# perform_cv(model, data$train)
-#
-#
-# data$test %>% dplyr::mutate_all(as.factor) %>% str
-
-source("cleantest.R")
 data <- get_data("babies23.data")
-t <- data$train
-head(t)
-hist(as.numeric(t$mrace))
 
 model <- logical_model(data$train)
-summary(model)
 mse <- calculate_MSE(model, data$test)
 print(mse)
-perform_cv(model, data$train)
 
 # Does model analysis
-plot(model)
+plot(model$finalModel)
