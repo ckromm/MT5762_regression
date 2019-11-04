48b3b08823b882e7b6e1b04385c9888d78c37349
ckromm <56156226+ckromm@users.noreply.github.com>
2019-11-04 01:43:50 +0000

    Added comments and changed names.

--- 5762_project2/spiffy_bootstrap_fun.R
+++ 5762_project2/spiffy_bootstrap_fun.R
@@ -1,24 +1,25 @@
 #Bootstrap function
 
 #inputs:
-#dataset: dataframe in R.
+#dataset: dataframe in R. The data be invertable.
 #nsim: The number of bootstrapped simulations
 #yvar: the response variable of the linear regression of interest. 
+
 
 spiffy_boots <- function(dataset, nsim, yvar){
   
   #Bootstap function
-  bs_fun<-function(inputData, nsim, Yvariable ){
-
+  bs_fun<-function(df, nsim, response ){
+    
     # bootstrap:  resample data with replacement
-    boots <- (inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),])
+    boots <- (df[sample(1:nrow(df), nrow(df), replace = T),])
     
     # fit the bootstrapped model 
-    regress <- as.formula( paste(Yvariable, "~", "."))
+    regress <- as.formula( paste(response, "~", "."))
     beta <- coef(lm(regress, data= boots))
   }
   
-  #boot_beta <- parSapply(cluster, 1:nsim, par_fun, bData = Data, Yvariable= Yvar)
+  #run the boot function
   betas_df <- sapply(1:nsim,  function(s) bs_fun(dataset, nsim, yvar))
   boot_df <-as.data.frame(t(betas_df))
   
@@ -26,20 +27,24 @@ spiffy_boots <- function(dataset, nsim, yvar){
   rownames(boot_df) <- c(1:nsim)
   
   #colnames
+  # run the model 
   regress <- as.formula( paste(yvar, "~", "."))
-  names_coef <- names(coef(lm( regress, data= dataset)))
+  names_coef <- names(coef(lm(regress, data= dataset)))
   
   colnames(boot_df) <- names_coef
   
-  #get c.i.
+  #name c.i.
   boot_ci <-as.data.frame(t(sapply( 1:ncol(boot_df), function(b) quantile(boot_df[ , b], probs = c(0.025, 0.975))))) 
   
-  
-  #name c.i.
   rownames(boot_ci) <- names_coef
   
   return(list(ci= boot_ci, betas = boot_df ))
   
 }
 
+
+
+fit3data<-cbind(fit3Data, train$wt)
+#run of fit 3.
+aa<-spiffy_boots(fit3data,100, "wt.1")
 
